b460370deb0019ca5303e3dab8d3f29fe9229afa
Ujjwal151 <ujjwalojha151@gmail.com>
2021-08-12 23:46:46 +0600

    commit

---
+++ CSharp Class Practise/Class 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.name = "Ujjwal";
+            st1.SetAge(22);
+            st1.Cgpa = 3.50;
+            st1.mob = "01792427407";
+
+
+            Console.WriteLine(st1.name);
+            Console.WriteLine(st1.GetAge());
+            Console.WriteLine(st1.Cgpa);
+            Console.WriteLine(st1.mob);
+
+            string str;
+            
+        }
+    }
+}
---
+++ CSharp Class Practise/Class 7/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_7
+{
+    class Student
+    {
+        public string name; // It can ba accessed anywhere in the project
+
+        private int age;     //We used Get and set Method to accesss the private fields
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetAge(int newAge)
+        {
+            age = newAge;
+        }
+
+        // We used property for cgpa
+        private double cgpa; 
+        public double Cgpa // Case Matters. 
+        {
+            get
+            {
+                return cgpa;
+            }
+            set
+            {
+                cgpa = value;
+            }
+        }
+
+       
+        // We used auto property instead of property for mob
+        public string mob { get; set; } // Case doesnot matter for auto property.
+        //public string mob; // We could also use it instead of auto property
+        /* private string mob;
+         public string Mob
+         {
+             get
+             {
+                 return mob;
+             }
+             set
+             {
+                 mob = value;
+             }
+         }*/
+
+
+
+
+
+
+
+    }
+}
